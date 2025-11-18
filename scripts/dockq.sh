607d6264120e8de6f6ff1ccb92148e2eb04bdb80
Geoffrey Martin-Noble <Geoffrey.Martin-Noble@amd.com>
2025-11-18 14:50:42 -0800

    Script to run DockQ across a directory
    I made this work even if there's additional nesting in the output
    directory. This maintains compatibility with alphafold's default output
    for multiple inputs, but makes it possible to split up runs into
    multiple independent jobs, as long as the top-level directory has the
    protein complex name in it.

---
+++ scripts/dockq.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Runs DockQ
+
+set -euo pipefail
+
+output_dir="$1"
+reference_dir="$2"
+shift; shift;
+declare -a complexes=("$@")
+
+if (( ${#complexes[@]} == 0 )); then
+    echo "No complexes specified, processing all of them"
+    complexes=($(find "${output_dir}" -maxdepth 1 -type d -exec basename {} \;))
+fi
+echo "Processing ${#complexes[@]} complexes."
+
+for complex_name in "${complexes[@]}"; do
+    echo "Processing complex: ${complex_name}"
+
+    for model_pdb in $(find "${output_dir}/${complex_name}" -type f -name "*_model_*.pdb"); do
+        reference_pdb="${reference_dir}/${complex_name}.pdb"
+
+        # Run DockQ
+        dockq_output_json="${model_pdb%.pdb}_dockq.json"
+        DockQ --json "${dockq_output_json}" "${model_pdb}" "${reference_pdb}" > /dev/null
+
+        echo "DockQ results for ${model_pdb} saved to ${dockq_output_json}"
+    done
+done
